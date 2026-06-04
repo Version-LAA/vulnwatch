220b8502b05ee7c8fb9d935ced49f845a16a6990
Latoya Alford <lat.jamison@gmail.com>
2026-06-03 23:41:44 -0400

    migrated db

--- vulnerabilities/views.py
+++ vulnerabilities/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Vulnerability
 # Create your views here.
+
+
+class VulnerabilityListView(ListView):
+    model = Vulnerability
+    template_name = 'home.html'
