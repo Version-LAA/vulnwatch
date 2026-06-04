79f4d253d66f0266294bbcfe51f34d571a281d0c
Latoya Alford <lat.jamison@gmail.com>
2026-06-04 19:42:45 -0400

    update view and template with vuln content

--- vulnerabilities/views.py
+++ vulnerabilities/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import Avg, Count, Max, Q
 
 from .models import Vulnerability
 # Create your views here.
@@ -8,3 +9,15 @@ from .models import Vulnerability
 class VulnerabilityListView(ListView):
     model = Vulnerability
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filtered_vulnerabilities'] = Vulnerability.objects.filter(
+            cvss_score__isnull=False)
+        stats = Vulnerability.objects.aggregate(
+            avg_cvss=Avg('cvss_score'),
+            total=Count('id'),
+            critical_count=Count('id', filter=Q(cvss_score__gte=9.0))
+        )
+        context['stats'] = stats
+        return context
