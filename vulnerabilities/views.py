b4795df9c89db397b662221802d3e6281104e897
Latoya Alford <lat.jamison@gmail.com>
2026-06-07 17:00:36 -0400

    update homepage metrics

--- vulnerabilities/views.py
+++ vulnerabilities/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.db.models import Avg, Count, Max, Q
 from django.core.paginator import Paginator
+from datetime import timedelta
+from django.utils import timezone
 
 from .models import Vulnerability
 # Create your views here.
@@ -23,10 +25,13 @@ class VulnerabilityListView(ListView):
         context['paginator'] = paginator
         # context['filtered_vulnerabilities'] = Vulnerability.objects.filter(
         #     cvss_score__isnull=False)
+        seven_days_ago = timezone.now() - timedelta(days=7)
         stats = Vulnerability.objects.aggregate(
             avg_cvss=Avg('cvss_score'),
             total=Count('id'),
-            critical_count=Count('id', filter=Q(cvss_score__gte=9.0))
+            critical_count=Count('id', filter=Q(
+                cvss_score__gte=9.0, published_date__gte=seven_days_ago))
         )
+
         context['stats'] = stats
         return context
