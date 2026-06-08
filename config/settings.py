6732f0e4469c28a2192cf09eb2d093f1752d8284
Latoya Alford <lat.jamison@gmail.com>
2026-06-08 19:16:38 -0400

    update setttings

--- config/settings.py
+++ config/settings.py
@@ -188,8 +188,9 @@ LOGGING = {
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
