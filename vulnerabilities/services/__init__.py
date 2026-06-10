f8376abf2e7b2a75c2428bb58d9793f46d64ede1
Latoya Alford <lat.jamison@gmail.com>
2026-06-10 13:34:52 -0400

    define epss fetch

--- vulnerabilities/services/__init__.py
+++ vulnerabilities/services/__init__.py
@@ -1,2 +1,3 @@
 from .nvd_service import obtain_nvd, parse_nvd_data, save_nvd_data
 from .cve_org_service import obtain_updates, save_cve_org_data, fetch_cve_data
+from .epss_service import fetch_epss_score
